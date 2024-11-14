087b240e403efa08942a197161a705cb10410b69
DaniMP84 <xDani84x@gmail.com>
2024-11-14 16:13:40 +0100

    Scanner y variables necesarias

---
+++ src/ElPenjat1/ElPenjat.java
@@ -0,0 +1,29 @@
+package ElPenjat1;
+import java.util.Scanner;
+public class ElPenjat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner entrada = new Scanner(System.in);
+		String palabra;
+		int jugadores;
+		int rondas;
+		char letra;
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
