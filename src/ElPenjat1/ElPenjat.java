c559b60aad7007f5cb34b567082dc1579c5908e2
DaniMP84 <xDani84x@gmail.com>
2024-11-14 16:49:12 +0100

    Bienvenida y boceto for
    La bienvenida que me habia olvidado y un boceto del bucle for contando las rondas.

--- src/ElPenjat1/ElPenjat.java
+++ src/ElPenjat1/ElPenjat.java
@@ -5,14 +5,23 @@ public class ElPenjat {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		
+		//Scanner y variables a usar + array.
 		Scanner entrada = new Scanner(System.in);
 		String palabra;
 		int jugadores;
 		int rondas;
 		char letra;
-		String[] palabrasParaElegir = {"Casa", "Lobo", "Caramelo", "Papel", "Perro", "Gato", "Ordenador", "Castaña",
+		String[] palabrasParaEelegir = {"Casa", "Lobo", "Caramelo", "Papel", "Perro", "Gato", "Ordenador", "Castaña",
 				"Pantano", "Rock"};
 		
+		
+		//Bienvenida y pciones para el usuario.
+		System.out.println("***************************************************************");
+		System.out.println("*Benvingut Al Joc Del Penjat*");
+		System.out.println("***************************************************************");
+		System.out.println("");
+		System.out.println("");
 		System.out.println("¿Que palabra eliges?");
 		palabra = entrada.nextLine();
 		System.out.println("¿Cuantos jugadores habrá?");
@@ -22,6 +31,14 @@ public class ElPenjat {
 		System.out.println("Elige una letra");
 		letra = entrada.next().charAt(0);
 		
+		
+		for(int i = 0; i < rondas;i++){
+			if (letra == palabrasParaEelegir[0]){
+			System.out.println(i);	
+			}i++;
+			
+			
+		}
 		
 		
 		
