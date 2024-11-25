a1cd52f2137621e583af1b78af529f717010f24e
DaniMP84 <xDani84x@gmail.com>
2024-11-25 11:52:18 +0100

    Creación de arrays y preguntar nombres
    He creado un array para guardar las letras que inicialmente estará compuesto de '_' y he creado un array que guardara el número de rondas ganadas por cada usuario.
    Además me habia olvidado de preguntar los nombres de los jugadores, lo he implementado después de pedir el número de jugadores.

--- src/ElPenjat1/ElPenjat.java
+++ src/ElPenjat1/ElPenjat.java
@@ -31,7 +31,7 @@ public class ElPenjat {
 			System.out.println(i + ": " + palabrasParaElegir[i]);		
 		}		
 		indicePalabra = entrada.nextInt();
-		String PalabraSecreta = palabrasParaElegir[indicePalabra].toLowerCase();
+		String palabraSecreta = palabrasParaElegir[indicePalabra].toLowerCase();
 		
 		/*Solicito número de jugadores y le añado un error en caso de no elegir el mínimo que son 2 con el bucle while, mientras elija menos de 2 saldra el mensaje d error
 		guardo la respuesta en la variable jugadores.*/
@@ -42,15 +42,37 @@ public class ElPenjat {
 			jugadores = entrada.nextInt();
 		}
 		
-		System.out.println("¿Cuantas rondas jugaréis?");
-
-		System.out.println("Elige una letra");
-
 		
-		
-			
-			
+		/*Solicito el nombre de los jugadores y creo una array para guardarlos ahi.El bucle es tan grande como el número de jugadores, pedirá
+		 * cantidad de nombres en función de cuantos jugadores hayan indicado que hay.
+		 * Además uso el (i + 1) para que cuando pida nombre de jugadores empiece por el 1 y no por el 0*/
+		String[] nombreJugadores = new String[jugadores];
+		for(int i = 0; i < jugadores;i++) {
+			System.out.println("Introducir nombre del jugador" + (i + 1) + ": ");
+			nombreJugadores[i] = entrada.next();
 		}
+		
+		
+		
+		//Pregunto cuantas rondas quiere jugar y lo guardo en la variable rondas.
+		System.out.println("¿Cuantas rondas jugaréis?");
+		rondas = entrada.nextInt();
+		
+		//Array en donde se guarda el estado de la palabra secreta y bucle correspondiente.Inicialmente cada letra del array tendrá un '_'.
+		char[] estadoPalabra = new char[palabraSecreta.length()];
+		for(int i = 0; i < estadoPalabra.length;i++) {
+			estadoPalabra[i] = '_';
+		}
+		
+		
+		//Inicio el contador de rondas ganadas creando un array que almacenará el número de rondas ganadas por cada usuario.
+		int[] lasRondasGanadas = new int[jugadores];
+		
+		
+		
+			
+			
+		
 		
 		
 		
